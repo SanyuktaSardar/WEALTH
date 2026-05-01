424cb61e3e705b31e23fd5761d535330ba86e210
Sanyukta Sardar <ssanyukta16@gmail.com>
2026-05-02 01:45:40 +0530

    chore: update footer credits

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-blue-50 dark:bg-gray-900 py-12 border-t dark:border-gray-800">
               <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-400">
-                <p>made with ❤️ by Sanyukta and taniisha</p>
+                <p>Made with ❤️ by Sanyukta Sardar and Taniisha Chakraborty</p>
               </div>
             </footer>
           </ThemeProvider>
